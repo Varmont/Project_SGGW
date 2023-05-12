2a59b0971a906a4f795921c5ce40b08868183850
Your Namegit <simi.bielenda@gmail.com>
2023-05-12 17:25:02 +0200

    feature: podmianka app i dodanie create_db.py

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 # tworze aplikację flask odnoszącą się do nazwy projektu
 app=Flask(__name__)
 # lokalizacja i inicjalizacja bazy danych, /// relative path
-app.config['SQLAlchemy_DATABASE_URI'] = "sqlite:///biuropodrozy.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///biuropodrozy.db'
 db=SQLAlchemy(app)
-db.init_app(app)
 
 
-@app.route('/', methods=['POST, GET'])
+# add datamodel and database file
+
+
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
