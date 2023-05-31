ca7954ce0b063b129fe7a61b2ebee6e0d2300164
barto <bartosz.legowiak@gmail.com>
2023-05-31 12:31:43 +0200

    Dodanie ulubionych

--- app/trips/trip.py
+++ app/trips/trip.py
@@ -1,10 +1,13 @@
 from ..extensions import db
 
+
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    date_start = db.Column(db.DateTime, nullable=False)
-    date_end = db.Column(db.DateTime, nullable=False)
+    date_start = db.Column(db.Date, nullable=False)
+    date_end = db.Column(db.Date, nullable=False)
+    country = db.Column(db.String(30), nullable=False)
     city = db.Column(db.String(30), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(100), nullable=False)
