483771671784ce5ace5738d6905bd5c3cc36410c
barto <bartosz.legowiak@gmail.com>
2023-06-07 19:20:36 +0200

    Przyciski limitujace ilosc wyswietlanych ogloszen

--- app/trips/routes.py
+++ app/trips/routes.py
@@ -2,9 +2,9 @@ from .trip import Trip
 from ..user_management.user import user_trip
 from ..extensions import db
 from datetime import datetime
-from flask import Blueprint, render_template, request, url_for, redirect
+from flask import Blueprint, render_template, request, flash, url_for, redirect
 from flask_login import current_user
-from ..main.forms import SearchForm
+from ..main.forms import CityForm
 
 trips = Blueprint('trips', __name__, template_folder = 'templates')
 
@@ -34,23 +34,34 @@ def tripview(tripID):
 
 @trips.route('/follow?trip=<tripID>', methods=['GET', 'POST'])
 def follow(tripID):
-    trip = Trip.query.filter_by(id=tripID).first()
-    current_user.followed.append(trip)
-    db.session.commit()
-    return render_template('tripsite.html', trip=trip)
+    if current_user.is_authenticated:
+        if current_user.is_following(tripID):
+            trip = Trip.query.filter_by(id=tripID).first()
+            flash("Ta wycieczka jest już dodana do ulubionych.", "error")
+            return render_template('tripsite.html', trip=trip)
+        trip = Trip.query.filter_by(id=tripID).first()
+        current_user.followed.append(trip)
+        db.session.commit()
+        flash('Dodano wycieczkę do ulubionych.', 'mess')
+        return redirect(url_for('trips.tripview', tripID=tripID))
+    else:
+        flash("Musisz być zalogowany, aby dodać wycieczkę do ulubionych!", 'error')
+        return render_template('login.html')
 
 @trips.route('/unfollow?trip=<tripID>', methods=['GET', 'POST'])
 def unfollow(tripID):
     trip = Trip.query.filter_by(id=tripID).first()
     current_user.followed.remove(trip)
     db.session.commit()
-    return render_template('tripsite.html', trip=trip)
+    flash('Usunięto wycieczkę z ulubionych.', 'mess')
+    return redirect(url_for('trips.tripview', tripID=tripID))
 
 
 @trips.route('/favourites', methods=['GET'])
 def favourite():
     userID = request.args.get('user')
-    form = SearchForm()
+    form = CityForm()
     trips = db.session.execute(db.select(Trip).join(user_trip).where(user_trip.c.user_id == userID)).scalars()
+
     return render_template('favourites.html', trips=trips, form=form)
 
--- app/user_management/routes.py
+++ app/user_management/routes.py
@@ -1,7 +1,9 @@
 from .user import User
 from ..extensions import db
-from flask import Blueprint, render_template, request, url_for, redirect
+from flask import Blueprint, render_template, request, flash, url_for, redirect
 from flask_login import login_user, logout_user, login_required
+from urllib.parse import urlparse
+import re
 
 user_management = Blueprint('user_management', __name__, template_folder='templates')
 
@@ -15,13 +17,12 @@ def register():
             user = User(email=email, password=password)
             db.session.add(user)
             db.session.commit()
-            alert = "Udało Ci się założyć konto!" \
-                    "Możesz się zalogować"
-            return render_template('login.html', alert=alert)
+            flash("Udało Ci się założyć konto!\nMożesz się zalogować.", 'mess')
+            return redirect(url_for('user_management.login'))
         else:
             email = request.form['email']
-            alert = "Hasła różnią się od siebie!"
-            return render_template('register.html', alert=alert, last_email=email)
+            flash("Hasła różnią się od siebie!", 'error')
+            return render_template('register.html', last_email=email)
     return render_template('register.html')
 
 
@@ -31,18 +32,26 @@ def login():
         email = request.form['email']
         password = request.form['password']
         user = User.query.filter_by(email=email).first()
+        path = urlparse(request.referrer)
         if user and user.password == password:
-            login_user(user)
-            alert = "Udało Ci się zalogować!"
-            return render_template('index.html', alert=alert)
+            if 'follow' in path.path:
+                tripID = int(re.findall(r'\d+\b', path.path)[0])
+                login_user(user)
+                return redirect(url_for('trips.follow', tripID=tripID))
+            else:
+                login_user(user)
+                flash("Udało Ci się zalogować!", 'mess')
+                return redirect(url_for('main.index'))
+
         else:
-            alert = "Błędne dane logowania!"
-            return render_template('login.html', alert=alert)
+            flash("Błędne dane logowania!", 'error')
+            return redirect(url_for('user_management.login'))
     return render_template('login.html')
 
 @user_management.route('/logout')
 @login_required
 def logout():
     logout_user()
+    flash("Wylogowałeś się!", 'mess')
     return redirect(url_for('main.index'))
 
